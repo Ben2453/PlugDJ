eb927710374f86e7da1d6f0c8d672e12a2796e6b
Sidewinder <xSidewinderx@users.noreply.github.com>
2015-04-09 14:49:12 -0500

    Update WorkingChatLog.js

--- WorkingChatLog.js
+++ WorkingChatLog.js
@@ -1,7 +1,7 @@
  // Logs all messages & Commands into console.
  //Code by: Sidewinder
  //Room: https://plug.dj/plugmix
-  API.chatLog('ChatLog Now Running -Sidewinders Script')
+  API.sendChat('ChatLog Now Running -Sidewinders Script')
   API.on (API.CHAT, function (data) {
     	console.log(data);
              var a = data.message.toLowerCase();
