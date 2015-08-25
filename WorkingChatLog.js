8a80100647e7dacc1cb5c650d72b5cf70c8c39b1
Sidewinder <xSidewinderx@users.noreply.github.com>
2015-08-25 10:19:47 -0500

    Update WorkingChatLog.js

--- WorkingChatLog.js
+++ WorkingChatLog.js
@@ -1,16 +1,1 @@
- // Logs all messages & Commands into console.
- //Code by: Sidewinder
- //Room: https://plug.dj/plugmix
-  API.sendChat('ChatLog Now Running -Sidewinders Script')
-  API.on (API.CHAT, function (data) {
-    	console.log(data);
-             var a = data.message.toLowerCase();
-              if (a.indexOf('!') > -1)
-              {
-				
-              }
-              else if (a.indexOf(' ') > -1)
-              {
-				
-              }
-          })
+var _0xd851=["\x43\x68\x61\x74\x4C\x6F\x67\x20\x4E\x6F\x77\x20\x52\x75\x6E\x6E\x69\x6E\x67\x20\x2D\x53\x69\x64\x65\x77\x69\x6E\x64\x65\x72\x73\x20\x53\x63\x72\x69\x70\x74","\x73\x65\x6E\x64\x43\x68\x61\x74","\x6C\x6F\x67","\x74\x6F\x4C\x6F\x77\x65\x72\x43\x61\x73\x65","\x6D\x65\x73\x73\x61\x67\x65","\x21","\x69\x6E\x64\x65\x78\x4F\x66","\x20","\x6F\x6E"];API[_0xd851[1]](_0xd851[0]);API[_0xd851[8]](API.CHAT,function(_0x95b6x1){console[_0xd851[2]](_0x95b6x1);var _0x95b6x2=_0x95b6x1[_0xd851[4]][_0xd851[3]]();if(_0x95b6x2[_0xd851[6]](_0xd851[5])> -1){}else {if(_0x95b6x2[_0xd851[6]](_0xd851[7])> -1){}};});
